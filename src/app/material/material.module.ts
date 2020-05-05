477db127d0e9f67aff89d1022456359d9b57fabf
AngelJGR <ajguerreror@gmail.com>
2020-05-05 12:53:20 -0400

    Updating

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+const MaterialComponents:Array<any> = [
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [ MaterialComponents ],
+  exports: [ MaterialComponents ]
+})
+export class MaterialModule { }
